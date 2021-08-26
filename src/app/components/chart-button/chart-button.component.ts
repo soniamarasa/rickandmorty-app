c69b4d29e2d3b9b8a1f92fae8e42260f355bbe37
Sonia Mara De Sá <soniamarasa@hotmail.com>
2021-08-26 11:41:48 -0300

    estilizando body

--- src/app/components/chart-button/chart-button.component.ts
+++ src/app/components/chart-button/chart-button.component.ts
@@ -11,71 +11,8 @@ import { ModalChartComponent } from '../modal-chart/modal-chart.component';
   styleUrls: ['./chart-button.component.css'],
 })
 export class ChartButtonComponent implements OnInit {
-  @Input() chart: string | undefined;
-  @Input() key: string | undefined;
-  hasNextPage: boolean = true;
-  currentPage: number = 1;
-  apiResponse: any = undefined;
-  modalRef: BsModalRef | undefined;
+  constructor() { }
 
-  constructor(
-    public apiService: ApiService,
-    private modalService: BsModalService
-  ) {}
-
-  ngOnInit(): void {}
-
-  async openChartModal(): Promise<void> {
-    this.hasNextPage = true;
-    const endpoint = this.key === 'eps' ? 'episode' : 'location';
-    const apiList = this.key === 'eps' ? 'epList' : 'charList';
-    while (this.hasNextPage) {
-      this.apiResponse = await this.apiService.getEndpoints(
-        endpoint,
-        this.currentPage
-      );
-      if (this.apiResponse) {
-        this.apiResponse.results.forEach((item: any) =>
-          this.apiService[apiList].push(item)
-        );
-        this.apiResponse.info.next ? this.currentPage++ : this.stopRequesting();
-      }
-    }
-
-    let obj: any = {};
-    if (this.key === 'eps') {
-      this.apiService.epList.forEach((ep) => {
-        const season = ep.episode.substring(0, 3);
-        obj[season] ? obj[season]++ : (obj[season] = 1);
-      });
-    } else {
-      this.apiService.charList.forEach((local) => {
-        obj[local.name] = local.residents.length;
-      });
-    }
-
-    const initialState = {
-      obj,
-
-      title:
-        this.key === 'eps'
-          ? 'Total de Episódios por Temporada'
-          : 'Total de Personagens por Planeta',
-
-      chartLab:
-        this.key === 'eps'
-          ? 'Quantidade de Episódios'
-          : 'Quantidade de Personagens',
-    };
-
-    this.modalRef = this.modalService.show(ModalChartComponent, {
-      class: 'modal-dialog-top modal-lg',
-      initialState,
-    });
-  }
-
-  stopRequesting() {
-    this.currentPage = 1;
-    this.hasNextPage = false;
+  ngOnInit(): void {
   }
 }
--- src/app/modules/body/body.component.ts
+++ src/app/modules/body/body.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent implements OnInit {
+  hasNextPage: boolean = true;
+  currentPage: number = 1;
+  apiResponse: any = undefined;
 
-  constructor() { }
+  currentEpObj: any = {};
+  currentLocalObj: any = {};
 
-  ngOnInit(): void {
+  renderGraphs: boolean = false;
+
+  constructor(private apiService: ApiService) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.requestData('eps');
+    await this.requestData('character');
+    this.renderGraphs = true;
   }
 
+  stopRequesting() {
+    this.currentPage = 1;
+    this.hasNextPage = false;
+  }
+
+  async requestData(key: string) {
+    this.hasNextPage = true;
+    const endpoint = key === 'eps' ? 'episode' : 'location';
+    const apiList = key === 'eps' ? 'epList' : 'charList';
+    while (this.hasNextPage) {
+      this.apiResponse = await this.apiService.getEndpoints(
+        endpoint,
+        this.currentPage
+      );
+      if (this.apiResponse) {
+        this.apiResponse.results.forEach((item: any) =>
+          this.apiService[apiList].push(item)
+        );
+        this.apiResponse.info.next ? this.currentPage++ : this.stopRequesting();
+      }
+    }
+
+    key === 'eps' ? (this.currentEpObj = {}) : (this.currentLocalObj = {});
+    if (key === 'eps') {
+      this.apiService.epList.forEach((ep) => {
+        const season = ep.episode.substring(0, 3);
+        this.currentEpObj[season]
+          ? this.currentEpObj[season]++
+          : (this.currentEpObj[season] = 1);
+      });
+    } else {
+      this.apiService.charList.forEach((local) => {
+        this.currentLocalObj[local.name] = local.residents.length;
+      });
+    }
+  }
 }
